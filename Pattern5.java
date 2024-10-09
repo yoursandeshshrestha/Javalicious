31c52c8e4d3e9f245f50a2021bd5d8e7ba97dd30
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-10-09 12:12:46 +0530

    feat: add star pattern 5 implementation

---
+++ Pattern5.java
@@ -0,0 +1,25 @@
+/*Star Pattern 5
+* * * * *
+  * * * *
+    * * *
+      * *
+        *  
+*/
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = rows; i >= 1; i--) { 
+          for (int j = rows; j > i; j--) { 
+            System.out.print(" "); 
+          }
+          for (int k = 1; k <= i; k++) {
+            System.out.print("*"); 
+          }
+          System.out.println(" ");
+         }
+    }
+
+}
+
+
